d6b5b596bb727cb418734d3ecc4e7cc407464ca9
Julian Anderson <julian@jumpcut.com>
2021-02-18 14:18:01 +0700

    adding User model and migration

---
+++ migrations/20210218071541-create-user.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      businessId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'Businesses',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Users');
+  }
+};
