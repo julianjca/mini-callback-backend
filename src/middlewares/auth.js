832be4773965b840a9eb5238228cc1a0532e7842
Julian Anderson <julian@jumpcut.com>
2021-02-19 10:09:20 +0700

    adding auth middlewares to businesses and callbacks

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,23 +2,23 @@ const jwt = require('jsonwebtoken')
 
 const accessTokenSecret = process.env.JWT_SECRET
 
-const authenticateJWT = (req, res, next) => {
+// eslint-disable-next-line consistent-return
+const authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
       const token = authHeader.split(' ')[1];
 
-      // eslint-disable-next-line consistent-return
-      jwt.verify(token, accessTokenSecret, (err, user) => {
-          if (err) {
-              return res.status(403).json({
-                message: 'Unauthorized'
-              });
-          }
-
-          req.user = user;
-          next();
-      });
+      try {
+        const user = await jwt.verify(token, accessTokenSecret)
+        req.user = user;
+        next();
+      }
+      catch {
+        return res.status(403).json({
+          message: 'Unauthorized'
+        });
+      }
   } else {
       res.status(401).json({
         message: 'Unauthorized.'
